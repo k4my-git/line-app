757d52e012e43f796ffa1ded8ba4d48a0acfcae7
k4my <kmabara0229@icloud.com>
2024-06-10 18:25:28 +0900

    'fix'

--- schedule.py
+++ schedule.py
@@ -13,9 +13,9 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 
 def main():
     to = 'Cd4be544e806ee37f624dfe92d68d6267'
