b36caf4988b75fde5b0eb6554518fa37286ade33
k4my <kmabara0229@icloud.com>
2024-06-10 19:56:41 +0900

    'fix_othello'

--- app.py
+++ app.py
@@ -63,12 +63,12 @@ def handle_message(event):
             TextSendMessage(text='ok'))
     elif msg.text == "start":
         board = othello.initial_board()
-        othello.save_game(user_id, board, 'B')
+        database.save_game(user_id, board, 'B')
         flex_message = FlexSendMessage(alt_text="オセロ", contents=othello.board_to_flex(board))
         line_bot_api.reply_message(event.reply_token, flex_message)
 
     elif msg.text == "continue":
-        board, turn = othello.load_game(user_id)
+        board, turn = database.load_game(user_id)
         if board:
             flex_message = FlexSendMessage(alt_text="オセロ", contents=othello.board_to_flex(board))
             line_bot_api.reply_message(event.reply_token, flex_message)
@@ -79,7 +79,7 @@ def handle_message(event):
 @handler.add(PostbackEvent)
 def handle_postback(event):
     user_id = event.source.user_id
-    board, turn = othello.load_game(user_id)
+    board, turn = database.load_game(user_id)
 
     if not board:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text="ゲームを開始するには「start」と入力してください。"))
@@ -101,10 +101,10 @@ def handle_postback(event):
                 else:
                     result_message += "引き分けです"
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result_message))
-                othello.delete_game(user_id)
+                database.delete_game(user_id)
             else:
                 turn = 'W' if turn == 'B' else 'B'
-                othello.save_game(user_id, board, turn)
+                database.save_game(user_id, board, turn)
                 flex_message = FlexSendMessage(alt_text="オセロ", contents=othello.board_to_flex(board))
                 line_bot_api.reply_message(event.reply_token, flex_message)
         else:
